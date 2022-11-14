2921788e52cb914aedd6932c970648da317fb604
LordAnisthDev <anisthdev69@gmail.com>
2022-11-14 12:44:28 +0530

    Updated the contract addresses

--- src/helper/ContractHelper.ts
+++ src/helper/ContractHelper.ts
@@ -11,7 +11,7 @@ export const contractData = (chainId: number): WriteContractArgs => {
 const getContractAddressByChainId = (chainId: number) => {
     switch (chainId) {
         case 80001:
-            return '0x4459B1562493Dd44346C86615d87Bf9376f130ae'
+            return '0xd05655443394E2600E47dFF79CF4D11376988c91'
         case 71401:
             return '0x2BE1f39bCBEDde8FF59Cb29B2b3D6F7A67E0EF44'
         // case 1313161555:
@@ -19,7 +19,7 @@ const getContractAddressByChainId = (chainId: number) => {
         // case 4:
         //     return '0x74FaD6886e8D7713053FC36C761EF11c40AD84cf'
         case 137:
-            return '0x823443514C68d06811A395CA917a2B43bA9DcC64'
+            return '0x4ddECbb3AD68C0f084Fb5BE98A612EbC71c95f0C'
         default:
             return '0x823443514C68d06811A395CA917a2B43bA9DcC64'
     }
