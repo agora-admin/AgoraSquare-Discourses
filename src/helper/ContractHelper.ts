49ffb717875ad014b395d630de1e90a935f51f5f
LordAnisthDev <anisthdev69@gmail.com>
2022-11-14 12:20:50 +0530

    Updated the smart contract addresses

--- src/helper/ContractHelper.ts
+++ src/helper/ContractHelper.ts
@@ -11,13 +11,13 @@ export const contractData = (chainId: number): WriteContractArgs => {
 const getContractAddressByChainId = (chainId: number) => {
     switch (chainId) {
         case 80001:
-            return '0x4459B1562493Dd44346C86615d87Bf9376f130ae'
+            return '0xd05655443394E2600E47dFF79CF4D11376988c91'
         // case 1313161555:
         //     return '0xDdeEa46a3b23Ef146dD9E6338Dd2136fF1a1C8cc'
         // case 4:
         //     return '0x74FaD6886e8D7713053FC36C761EF11c40AD84cf'
         case 137:
-            return '0x823443514C68d06811A395CA917a2B43bA9DcC64'
+            return '0x4ddECbb3AD68C0f084Fb5BE98A612EbC71c95f0C'
         default:
             return '0x823443514C68d06811A395CA917a2B43bA9DcC64'
     }
