e76cd6c8af096b11a73fd2d411d8ab71bec08243
LordAnisthDev <anisthdev69@gmail.com>
2022-11-25 19:29:16 +0530

    Added Claim NFT ui

--- src/components/utils/SEOHome.tsx
+++ src/components/utils/SEOHome.tsx
@@ -2,18 +2,18 @@ import Head from "next/head";
 const SEOHome = () => {
     return (
         <Head>
-            <meta name="description" content="Crowdfund thought leaders to engage in dialogue to inform governance, deliberation, and knowledge discovery." />
+            <meta name="description" content="Create crowdfunding campaigns to see thought leaders engage in dialogue." />
             <meta property="og:url" content="https://discourses.agorasquare.xyz/" />
             <meta property="og:type" content="website" />
-            <meta property="og:title" content="Discourses by Agora Square" />
-            <meta property="og:description" content="Crowdfund thought leaders to engage in dialogue to inform governance, deliberation, and knowledge discovery." />
+            <meta property="og:title" content="Discourses | AGORA SQUARE" />
+            <meta property="og:description" content="Create crowdfunding campaigns to see thought leaders engage in dialogue." />
             <meta property="og:image" content="https://discourses.agorasquare.xyz/og_s.png" />
 
             <meta name="twitter:card" content="summary_large_image" />
             <meta property="twitter:domain" content="discourses.agorasquare.xyz" />
             <meta property="twitter:url" content="https://discourses.agorasquare.xyz/" />
-            <meta name="twitter:title" content="Discourses by Agora Square" />
-            <meta name="twitter:description" content="Crowdfund thought leaders to engage in dialogue to inform governance, deliberation, and knowledge discovery." />
+            <meta name="twitter:title" content="Discourses | AGORA SQUARE" />
+            <meta name="twitter:description" content="Create crowdfunding campaigns to see thought leaders engage in dialogue." />
             <meta name="twitter:image" content="https://discourses.agorasquare.xyz/og_l.png" />
 
         </Head>
