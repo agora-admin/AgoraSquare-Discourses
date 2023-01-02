524e327d5d47403a2f3d38f537860e40086c3b5d
LordAnisthDev <anisthdev69@gmail.com>
2023-01-02 22:02:48 +0530

    Removed the env variable for client id

--- src/web3/Connectors.ts
+++ src/web3/Connectors.ts
@@ -1,7 +1,7 @@
 import UAuth from '@uauth/js'
 
 const uauth = new UAuth({
-  clientID: process.env.NEXT_PUBLIC_UNSTOPPABLE_CLIENT_ID as string,
+  clientID: "e481d54e-4645-4595-ae2a-f4cc09de6261",
   redirectUri: "http://localhost:3000",
   scope: "openid wallet"
 })
