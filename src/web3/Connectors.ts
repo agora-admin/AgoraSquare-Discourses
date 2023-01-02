3a3dc7ed4273a176c46af5b81251cdc0750a421e
LordAnisthDev <anisthdev69@gmail.com>
2023-01-02 22:07:35 +0530

    added redirectUri

--- src/web3/Connectors.ts
+++ src/web3/Connectors.ts
@@ -2,7 +2,7 @@ import UAuth from '@uauth/js'
 
 const uauth = new UAuth({
   clientID: "e481d54e-4645-4595-ae2a-f4cc09de6261",
-  redirectUri: "http://localhost:3000",
+  redirectUri: "https://discourses.agorasquare.xyz",
   scope: "openid wallet"
 })
 
