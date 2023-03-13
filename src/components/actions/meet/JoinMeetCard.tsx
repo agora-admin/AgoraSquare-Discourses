cb7e7c6a5d14b37160a66310a2936d66fc620a4a
sinhavishesh <visheshroxx.vs@gmail.com>
2023-03-13 18:51:09 +0530

    updated joinMeetCard flow

--- src/components/actions/meet/JoinMeetCard.tsx
+++ src/components/actions/meet/JoinMeetCard.tsx
@@ -1,10 +1,10 @@
-import { Clock, Verify } from "iconsax-react";
+import { Clock, Verify, ArrowCircleRight, ProfileCircle } from "iconsax-react";
 import { formatDate, getTimeFromDate, isDisputable, isFuture, isPast } from "../../../helper/TimeHelper";
 import { ArrowRightIcon, HappeningIconGreen } from "../../utils/SvgHub";
 import { GET_DISCOURSE_BY_ID, GET_TOKEN_BY_ID } from "../../../lib/queries";
 import { useContext, useEffect, useState } from "react";
 import { useRouter } from "next/router";
-import { discouresEnded } from "../../../helper/DataHelper";
+// import { discouresEnded } from "../../../helper/DataHelper";
 import { useLazyQuery, useMutation } from "@apollo/client";
 import { ENTER_DISCOURSE, RAISE_DISPUTE } from "../../../lib/mutations";
 import { useContractWrite, useNetwork, usePrepareContractWrite, useWaitForTransaction } from "wagmi";
@@ -96,20 +96,24 @@ const JoinMeetCard = ({ data }: { data: any }) => {
     }
 
     const handleJoinMeet = () => {
+        // setLoading(true);
+        // if (!userIsSpeaker()) {
+        //     if (token !== "" && !isPast(timeStamp) && propId === data.propID) {
+        //         route.push("/live/" + data.id)
+        //         setLoading(false);
+        //     } else {
+        //         getMeetToken();
+        //     }
+        // } else {
+        //     if (speakerEntered()) {
+        //         joinMeet();
+        //     } else {
+        //         enterDiscourse();
+        //     }
+        // }
         setLoading(true);
-        if (!userIsSpeaker()) {
-            if (token !== "" && !isPast(timeStamp) && propId === data.propID) {
-                route.push("/live/" + data.id)
-                setLoading(false);
-            } else {
-                getMeetToken();
-            }
-        } else {
-            if (speakerEntered()) {
-                joinMeet();
-            } else {
-                enterDiscourse();
-            }
+        if(!speakerEntered()) {
+            enterDiscourse();
         }
     }
 
@@ -237,58 +241,79 @@ const JoinMeetCard = ({ data }: { data: any }) => {
     }, [tokenData])
 
     return (
-        <>
-            {isMeetHappening() && isPast(d.meet_date) &&
-                <div className="bg-card rounded-xl p-4 flex flex-col gap-2">
-                    <div className="flex items-center gap-2">
-                        <HappeningIconGreen />
-                        <p className="text-gradient-g font-Lexend font-bold text-sm">Happening Now</p>
-                    </div>
-                    <p className="text-[#c6c6c6] text-[10px]">
-                        Discourse started at <b>{formatDate(new Date(d.meet_date))}</b> • <b>{getTimeFromDate(new Date(d.meet_date))}</b>
-                    </p>
-                    {loggedIn && <button onClick={handleJoinMeet} className="button-s flex items-center gap-2 w-max bg-gradient-g">
-                        <p className="text-gradient-g text-sm font-Lexend text-[#212427] font-medium">{loading ? 'wait..' : "join"}</p>
-                        {!loading && <ArrowRightIcon />}
-                    </button>}
-                    {
-                        !loggedIn &&
-                        <p className="text-yellow-200/70 text-[10px] font-medium bg-yellow-200/10 px-2 rounded-md mt-2 py-1">Connect your wallet to join the discourse.</p>
-                    }
-                </div>
-            }
+        // <>
+        //     {isMeetHappening() && isPast(d.meet_date) &&
+        //         <div className="bg-card rounded-xl p-4 flex flex-col gap-2">
+        //             <div className="flex items-center gap-2">
+        //                 <HappeningIconGreen />
+        //                 <p className="text-gradient-g font-Lexend font-bold text-sm">Happening Now</p>
+        //             </div>
+        //             <p className="text-[#c6c6c6] text-[10px]">
+        //                 Discourse started at <b>{formatDate(new Date(d.meet_date))}</b> • <b>{getTimeFromDate(new Date(d.meet_date))}</b>
+        //             </p>
+        //             {loggedIn && <button onClick={handleJoinMeet} className="button-s flex items-center gap-2 w-max bg-gradient-g">
+        //                 <p className="text-gradient-g text-sm font-Lexend text-[#212427] font-medium">{loading ? 'wait..' : "join"}</p>
+        //                 {!loading && <ArrowRightIcon />}
+        //             </button>}
+        //             {
+        //                 !loggedIn &&
+        //                 <p className="text-yellow-200/70 text-[10px] font-medium bg-yellow-200/10 px-2 rounded-md mt-2 py-1">Connect your wallet to join the discourse.</p>
+        //             }
+        //         </div>
+        //     }
 
-            {isFuture(d.meet_date) &&
-                <div className="bg-card rounded-xl p-4 flex flex-col gap-2">
-                    <div className="flex items-center gap-2">
-                        <Clock size='18' color="#c6c6c6" />
-                        <p className="text-[#c6c6c6] font-Lexend font-bold text-sm">Scheduled</p>
-                    </div>
-                    <p className="text-[#c6c6c6] text-[10px]">
-                        Discourse scheduled on <b>{formatDate(new Date(d.meet_date))}</b> at <b>{getTimeFromDate(new Date(d.meet_date))}</b>
-                    </p>
-                </div>
-            }
+        //     {isFuture(d.meet_date) &&
+        //         <div className="bg-card rounded-xl p-4 flex flex-col gap-2">
+        //             <div className="flex items-center gap-2">
+        //                 <Clock size='18' color="#c6c6c6" />
+        //                 <p className="text-[#c6c6c6] font-Lexend font-bold text-sm">Scheduled</p>
+        //             </div>
+        //             <p className="text-[#c6c6c6] text-[10px]">
+        //                 Discourse scheduled on <b>{formatDate(new Date(d.meet_date))}</b> at <b>{getTimeFromDate(new Date(d.meet_date))}</b>
+        //             </p>
+        //         </div>
+        //     }
 
-            {
-                isPast(d.meet_date) && !isMeetHappening() && discouresEnded(data) &&
-                <div className="bg-card rounded-xl p-4 flex flex-col gap-2">
-                    <div className="flex items-center gap-2">
-                        <Verify size='18' color="#ABECD6" />
-                        <p className="text-[#ABECD6] font-Lexend text-sm">Happened</p>
-                    </div>
-                    <p className="text-[#c6c6c6] text-[10px]">
-                        Discourse completed on <b>{formatDate(new Date(d.meet_date))}</b>
-                    </p>
-                    {
-                        isDisputable(new Date(d.c_timestamp)) &&
-                        <div className="flex items-center">
-                            <p className="text-[#797979] font-semibold text-[10px]"></p><button onClick={() => raiseDispute()} className="text-[#FC8181] font-semibold text-[10px] outline-none border-none hover:underline">Raise Dispute </button>
-                        </div>
-                    }
-                </div>
-            }
-        </>
+        //     {
+        //         isPast(d.meet_date) && !isMeetHappening() && discouresEnded(data) &&
+        //         <div className="bg-card rounded-xl p-4 flex flex-col gap-2">
+        //             <div className="flex items-center gap-2">
+        //                 <Verify size='18' color="#ABECD6" />
+        //                 <p className="text-[#ABECD6] font-Lexend text-sm">Happened</p>
+        //             </div>
+        //             <p className="text-[#c6c6c6] text-[10px]">
+        //                 Discourse completed on <b>{formatDate(new Date(d.meet_date))}</b>
+        //             </p>
+        //             {
+        //                 isDisputable(new Date(d.c_timestamp)) &&
+        //                 <div className="flex items-center">
+        //                     <p className="text-[#797979] font-semibold text-[10px]"></p><button onClick={() => raiseDispute()} className="text-[#FC8181] font-semibold text-[10px] outline-none border-none hover:underline">Raise Dispute </button>
+        //                 </div>
+        //             }
+        //         </div>
+        //     }
+        // </>
+        <div className="mobile:fixed mobile:bottom-[60px] mobile:inset-x-0 mobile:max-h-[220px] flex flex-col sm:flex-row items-center mobile:gap-4 sm:justify-between py-6 sm:py-3 px-6 bg-[#141414] sm:border-[1.2px] sm:border-[#E5F7FF] rounded-t-[30px] sm:rounded-3xl">
+        <div className="flex flex-col sm:flex-row items-center gap-1 sm:gap-3">
+            <div className="mobile:hidden">
+                <ProfileCircle size="34" color="#84B9D1" variant="Bulk" />
+            </div>
+
+            <div className="sm:hidden">
+                <ProfileCircle size="50" color="#84B9D1" variant="Bulk" />
+            </div>
+
+            <div className="flex flex-col">
+                <h4 className="text-[#84B9D1] mobile:text-center font-bold text-[13px]">Speaker Attendance</h4>
+                <small className="text-[11px] text-[#E5F7FFE5] mobile:text-center font-semibold">Connect your wallet with your twitter account and confirm your attendance</small>
+            </div>
+        </div>
+        
+        <button disabled={loading} onClick={handleJoinMeet} className="flex items-center gap-2 bg-[#84B9D1] rounded-2xl p-3 cursor-pointer">
+            <span className="text-black text-xs font-Lexend font-medium">{loading ? "Please wait..." : "Join"}</span>
+            <ArrowCircleRight color="#4F6F7D" variant="Bulk" fill="#000"/>
+        </button>
+    </div>
     );
 }
 
