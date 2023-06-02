43adb8a489cb3f7c59b66a65e54e980770da1069
sinhavishesh <visheshroxx.vs@gmail.com>
2023-06-02 21:58:32 +0530

    udpate connector.ts

--- src/web3/Connectors.ts
+++ src/web3/Connectors.ts
@@ -2,7 +2,7 @@ import UAuth from '@uauth/js'
 
 const uauth = new UAuth({
   clientID: process.env.NEXT_PUBLIC_UNSTOPPABLE_CLIENT_ID as string,
-  redirectUri: process.env.NEXT_PUBLIC_CLIENT_URL as string,
+  redirectUri: process.env.NEXT_PUBLIC_SERVER_URL as string,
   scope: "openid wallet"
 })
 
